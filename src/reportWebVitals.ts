86e7ad9bcae0b3e51fadf66c37f9484079f1985e
Tomohiro Ubukata <ykmr729@gmail.com>
2022-10-13 21:36:11 +0900

    Add consola

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,15 @@
+import consola from "consola"
 import { ReportHandler } from 'web-vitals'
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {  // eslint-disable-line
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry)
       getFID(onPerfEntry)
       getFCP(onPerfEntry)
       getLCP(onPerfEntry)
       getTTFB(onPerfEntry)
-    })
+    }).catch(error => consola.error(error))
   }
 }
 
