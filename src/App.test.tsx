1284bc9fd423d6da2eb75e139ee9250dc5cb8990
Tomohiro Ubukata <ykmr729@gmail.com>
2022-10-14 21:34:11 +0900

    Change App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,14 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const headerElement = screen.getByText('redichan');
+  const paragraphElement1 = screen.getByText('redichan is a newborn social media. Please cheer us and bring your friends!');
+  const paragraphElement2 = screen.getByText(`You're not traced down from other users because your user name is not shown.`);
+  const paragraphElement3 = screen.getByText('Malicious users are suspended because redichan requires registration to post.');
+  const paragraphElement4 = screen.getByText('redichan suppoorts latest version of Google Chrome and Safari.');
+  expect(headerElement).toBeInTheDocument();
+  expect(paragraphElement1).toBeInTheDocument();
+  expect(paragraphElement2).toBeInTheDocument();
+  expect(paragraphElement3).toBeInTheDocument();
+  expect(paragraphElement4).toBeInTheDocument();
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const App = (): JSX.Element => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <h1>redichan</h1>
+      <p>redichan is a newborn social media. Please cheer us and bring your friends!</p>
+      <p>You&apos;re not traced down from other users because your user name is not shown.</p>
+      <p>Malicious users are suspended because redichan requires registration to post.</p>
+      <p>redichan suppoorts latest version of Google Chrome and Safari.</p>
   </div>);
 
 export default App
