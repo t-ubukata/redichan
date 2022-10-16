ddfcb998be0d2014cfd51a1549c42d7b691e9b19
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2022-10-16 15:08:43 +0900

    Refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,21 +5,21 @@ import App from './App'
 test('renders learn react link', () => {
   render(<App />)
   const headerElement = screen.getByText('redichan')
-  const paragraphElement1 = screen.getByText(
+  const pElement1 = screen.getByText(
     'redichan is a newborn social media. Please cheer us and bring your friends!'
   )
-  const paragraphElement2 = screen.getByText(
+  const pElement2 = screen.getByText(
     `You're not traced down from other users because your user name is not shown.`
   )
-  const paragraphElement3 = screen.getByText(
+  const pElement3 = screen.getByText(
     'Malicious users are suspended because redichan requires registration to post.'
   )
-  const paragraphElement4 = screen.getByText(
+  const pElement4 = screen.getByText(
     'redichan suppoorts latest version of Google Chrome and Safari.'
   )
   expect(headerElement).toBeInTheDocument()
-  expect(paragraphElement1).toBeInTheDocument()
-  expect(paragraphElement2).toBeInTheDocument()
-  expect(paragraphElement3).toBeInTheDocument()
-  expect(paragraphElement4).toBeInTheDocument()
+  expect(pElement1).toBeInTheDocument()
+  expect(pElement2).toBeInTheDocument()
+  expect(pElement3).toBeInTheDocument()
+  expect(pElement4).toBeInTheDocument()
 })
