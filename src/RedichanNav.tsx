03c0fdae1baa3985cd473be3fac0da152c906f25
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2022-10-23 17:53:04 +0900

    Add fetching boards

--- src/RedichanNav.tsx
+++ src/RedichanNav.tsx
@@ -1,36 +1,57 @@
+import {useState, useEffect} from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import consola from'consola';
 import LOGO from './logo.svg';
 
-const RedichanNav = (): JSX.Element => (
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Navbar.Brand href="/">
-      <img src={LOGO} width="30" height="30" alt="React Bootstrap logo" />
-      redichan
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="mr-auto">
-        <NavDropdown
-          title="English"
-          id="navbarScrollingDropdown"
-          data-testid="en-boards"
-        >
-          <NavDropdown.Item href="#en-news">
-            News
-          </NavDropdown.Item>
-          <NavDropdown.Item href="#en-sensitive-may">
-            Sensitive may
-          </NavDropdown.Item>
-        </NavDropdown>
-        <NavDropdown title="日本語" id="navbarScrollingDropdown" data-testid="ja-boards" >
-          <NavDropdown.Item href="#ja-news">ニュース</NavDropdown.Item>
-          <NavDropdown.Item href="#ja-sensitive-may">裏may</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+interface EnBoards {
+  id: number
+  name: string
+  path: string
+}
+
+const RedichanNav = (): JSX.Element => {
+
+  const [enBoards, setEnBoards] = useState({});
+
+  useEffect(() => {
+    const fetchEnBoards = async () => {
+      const response = await fetch('/api/en-boards');
+      const json = await response.json() as EnBoards;
+      setEnBoards(json);
+    };
+    fetchEnBoards().catch(err => consola.error(err));
+  });
+
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <img src={LOGO} width="30" height="30" alt="React Bootstrap logo" />
+        redichan
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <NavDropdown
+            title="English"
+            id="navbarScrollingDropdown"
+            data-testid="en-boards"
+          >
+            <NavDropdown.Item href="#en-news">
+              News
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#en-sensitive-may">
+              Sensitive may
+            </NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="日本語" id="navbarScrollingDropdown" data-testid="ja-boards" >
+            <NavDropdown.Item href="#ja-news">ニュース</NavDropdown.Item>
+            <NavDropdown.Item href="#ja-sensitive-may">裏may</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>);
+};
 
 export default RedichanNav;
