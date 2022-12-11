feaf8547f877d4d3050282976cafb5f670cb8ca2
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2022-12-11 17:51:19 +0900

    Remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'index.css';
 import Home from './Home';
 import Board from './Board';
