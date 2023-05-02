47142c77c03c0c172c09f0f68d3afbda279f209d
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-02 19:04:55 +0900

    Fix Thread.test.tsx

--- src/Thread.test.tsx
+++ src/Thread.test.tsx
@@ -1,20 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Thread from './Thread';
 
 test('renders thread', () => {
   render(
-    <BrowserRouter>
+    <MemoryRouter initialEntries={["/thread/0"]}>
       <Routes>
-        <Route path="/thread/0" element={<Thread />} />
+        <Route path="/thread/:id" element={<Thread />} />
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 
-  /*
-  const post= screen.getByText('➕');
+  const post = screen.getByText('➕');
 
   expect(post).toBeInTheDocument();
-  */
 });
