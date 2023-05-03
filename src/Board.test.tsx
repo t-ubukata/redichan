313346e921729e96ebcb371de2ad26b8def4b619
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-03 17:35:13 +0900

    Fix Board.test.tsx

--- src/Board.test.tsx
+++ src/Board.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import Board from './Board';
 
 test('renders Board', () => {
-  render(<Board name="enNews" />);
+  render(
+    <MemoryRouter initialEntries={['/board/en/news']}>
+      <Routes>
+        <Route path="/board/en/news" element={<Board name="enNews" />} />
+      </Routes>
+    </MemoryRouter>
+  );
 
   const search = screen.getByText('🔍');
   const startThread = screen.getByText('➕');
