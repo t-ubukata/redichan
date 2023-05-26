66f7578f4f7a575f395dba5867359b856afdd040
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-26 19:57:08 +0900

    Fix data_access.py

--- redichan_api/redichan_api/redichan_api/data_access.py
+++ redichan_api/redichan_api/redichan_api/data_access.py
@@ -2,7 +2,7 @@ from django.db import connection
 
 def get_tables(self, lang: string):
     with connection.cursor() as cursor:
-        q = 'SELECT id, name, path FROM redichan.tables WHERE language = %s' \
+        q = 'SELECT id, name, path FROM redichan.tables WHERE language = %s ' \
         'ORDER BY order_in_lang'
         cursor.execute(q, [lang])
         row = cursor.fetchall()
