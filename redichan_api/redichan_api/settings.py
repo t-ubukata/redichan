50a1831e83f58c43474c384569fcff280234b4b1
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-28 13:10:13 +0900

    Refactor

--- redichan_api/redichan_api/settings.py
+++ redichan_api/redichan_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-c_t$@8r%@3gh5i+$&)_uz9ev+h&_+t0ccu-l1s%q*b7$%^+cg6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS : List[str]= []
 
 
 # Application definition
