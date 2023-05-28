2d7afda583f6fee4a1a98436ef0bbab2b1170db0
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-28 11:51:42 +0900

    Change format

--- redichan_api/redichan_api/redichan_api/data_access.py
+++ redichan_api/redichan_api/redichan_api/data_access.py
@@ -3,7 +3,7 @@ from django.db import connection
 def get_tables(self, lang: string):
     with connection.cursor() as cursor:
         q = 'SELECT id, name, path FROM redichan.tables WHERE language = %s ' \
-        'ORDER BY order_in_lang'
+            'ORDER BY order_in_lang'
         cursor.execute(q, [lang])
         row = cursor.fetchall()
 
