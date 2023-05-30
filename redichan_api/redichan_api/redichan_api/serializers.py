801a2119b36917614bc58dc1acef8310f173a884
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-30 13:25:17 +0900

    Add serializers.py

---
+++ redichan_api/redichan_api/redichan_api/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import Boards
+from rest_framework import serializers
+
+
+class BoardsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Boards
+        fields: list[str] = ['id', 'name', 'path']
+
