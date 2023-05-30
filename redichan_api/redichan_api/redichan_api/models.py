0e684a907619179b84559885745869df2730c7d9
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-30 13:24:45 +0900

    Change data_access.py to models.py

--- redichan_api/redichan_api/redichan_api/models.py
+++ redichan_api/redichan_api/redichan_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.query import RawQuerySet
 
-# Create your models here.
+
+class Boards(model.Model):
+  id: models.IntegerField = models.IntegerField()
+  name: models.CharField = models.CharField(32)
+  path: models.CharField = models.CharField(64)
+
+  def select(self, lang: str) -> RawQuerySet:
+    q = 'SELECT id, name, path FROM redichan.tables WHERE language = %s ' \
+        'ORDER BY order_in_lang'
+    return Person.objects.raw(q, [lang])
