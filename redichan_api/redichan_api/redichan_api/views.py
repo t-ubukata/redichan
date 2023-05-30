6068c9c6d1dfb20cdb5b389373af2daecc17a371
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-30 13:58:20 +0900

    Change views.py

--- redichan_api/redichan_api/redichan_api/views.py
+++ redichan_api/redichan_api/redichan_api/views.py
@@ -2,14 +2,18 @@ from django.db.models.query import RawQuerySet
 from django.contrib.auth.models import Boards
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.response import Response
 from redichan_api.redichan_api.serializers import BoardsSerializer
 
 
 class EnBoardsViewSet(viewsets.ModelViewSet):
+  def list(self, request):
     queryset: RawQuerySet = Boards.select('en')
     serializer_class = BoardsSerializer
+    return Response(serializer.data)
+
 
 class JaBoardsViewSet(viewsets.ModelViewSet):
+  def list(self, request):
     queryset: RawQuerySet = Boards.select('ja')
     serializer_class = BoardsSerializer
-
