c16b8a0401cf21c55c29453ee8cef410b85d0840
Tomohiro Ubukata <tomohiro.ubukata@gmail.com>
2023-05-30 13:44:17 +0900

    Add Views and URLs

--- redichan_api/redichan_api/redichan_api/views.py
+++ redichan_api/redichan_api/redichan_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.db.models.query import RawQuerySet
+from django.contrib.auth.models import Boards
+from rest_framework import viewsets
+from rest_framework import permissions
+from redichan_api.redichan_api.serializers import BoardsSerializer
 
-# Create your views here.
+
+class EnBoardsViewSet(viewsets.ModelViewSet):
+    queryset: RawQuerySet = Boards.select('en')
+    serializer_class = BoardsSerializer
+
+class JaBoardsViewSet(viewsets.ModelViewSet):
+    queryset: RawQuerySet = Boards.select('ja')
+    serializer_class = BoardsSerializer
+
--- redichan_api/redichan_api/urls.py
+++ redichan_api/redichan_api/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from redichan_api.redichan_api import views
+
+router = routers.DefaultRouter()
+router.register(r'api/en/boards', views.EnBoardsViewSet)
+router.register(r'api/ja/boards', views.JaBoardsViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
