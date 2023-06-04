2565c7988cffc3b77ee3aa96567eb4a07c9aaec4
Tomohiro Ubukata <ykmr729@gmail.com>
2023-06-04 17:19:05 +0900

    Add a test

--- redichan_api/redichan_api/redichan_api/tests.py
+++ redichan_api/redichan_api/redichan_api/tests.py
@@ -1,3 +1,17 @@
+from django.db.models.query import RawQuerySet
 from django.test import TestCase
+from redichan_api.redichan_api import models
+from unittest import mock
 
-# Create your tests here.
+
+class BoardsTests(TestCase):
+
+  raw_result = RawQuerySet('')
+  raw_result._result_cache = {id: 1, name: 'News', path: '/boards/en/news'}
+
+  @mock.patch('models.Boards.objects.raw',
+              mock.MagicMock(return_value=raw_result))
+  def test_select_returns_id_name_path(self):
+    b = models.Boards()
+    result = b.select('en')
+    print(result)
