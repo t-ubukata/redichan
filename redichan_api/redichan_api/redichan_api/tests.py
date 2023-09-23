7770b0c86433c99e144d02aa945c3cc7a4ad5873
Tomohiro Ubukata <ykmr729@gmail.com>
2023-09-23 17:08:34 +0900

    Edit for Stack Overflow

--- redichan_api/redichan_api/redichan_api/tests.py
+++ redichan_api/redichan_api/redichan_api/tests.py
@@ -9,47 +9,18 @@ from unittest.mock import patch, PropertyMock
 import traceback
 
 
-class BoardTests(TestCase):
-
-  def test_get_returns_id_name_path(self):
-    q = '''
-        SELECT board_id, name, path FROM redichan.boards
-          WHERE language = %s
-          ORDER BY order_in_lang
-        '''
-
-    # Django doesn't execute a query here, just holds a raw query string
-    # in a RawQuery object.
-    result = models.Board.get('en')
-
-    self.assertEqual(result.raw_query, q)
-
-
-class BoardSerializerTests(TestCase):
-
-  def test_constructor_returns_list_serializer_object(self):
-    raw_query_set = models.Board.get('en')
-    # When many=True is specified, the constructor returns
-    # a ListSerializer object.
-    boards_serializer = serializers.BoardSerializer(raw_query_set, many=True)
-
-    self.assertIsInstance(boards_serializer, ListSerializer)
-
-
 class EnBoardsViewSetTest(TestCase):
 
   @patch('rest_framework.serializers.BaseSerializer.data')
   def test_get_returns_en_boards(self, mocked_data):
-    # expected = [
-    #   {'lang': 'en', 'name': 'News', 'path': '/boards/en/news',
-    #    'minutes_to_live': 1440, 'order_in_lang': 1},
-    #   {'lang': 'en', 'name': 'Politics', 'path': '/boards/en/politics',
-    #    'minutes_to_live': 1440, 'order_in_lang': 2},
-    # ]
+    expected = [
+      {'lang': 'en', 'name': 'News', 'path': '/boards/en/news',
+       'minutes_to_live': 1440, 'order_in_lang': 1},
+      {'lang': 'en', 'name': 'Politics', 'path': '/boards/en/politics',
+       'minutes_to_live': 1440, 'order_in_lang': 2},
+    ]
 
-    expected = []
-
-    mocked_data = PropertyMock(return_value=[])
+    mocked_data = PropertyMock(return_value=expected)
 
     req = Request(HttpRequest())
     view_set = views.EnBoardsViewSet()
